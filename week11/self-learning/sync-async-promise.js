b5af2e918d0be94bacbb9d48545379a80eabf6f3
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-11-08 13:23:42 +0700

    More practice at home

--- week11/self-learning/sync-async-promise.js
+++ week11/self-learning/sync-async-promise.js
@@ -6,27 +6,58 @@
 สิ่งที่ต้องทำ : เรียกใช้ด้วย .then().catch()
 แสดงผลเป็น: ถ้าสำเร็จ → "Download success!"
            ถ้าล้มเหลว → "Network Error!" */
-async function downloadFile(isNetworkError){
+// async function downloadFile(isNetworkError){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(isNetworkError) {
+//                 reject("Network Error!")
+//             } else {
+//                 resolve("Download success!")
+//             }
+//         }, 2000);
+//     })
+// }
+
+// console.log("Start...");
+
+// downloadFile(false)
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+//     .finally(() => {
+//         console.log("Finish");
+//     });
+
+/*Exercise 2 (ปานกลาง): ใช้ async-await จัดการ Error
+ให้แก้โค้ดข้อ 1 มาใช้แบบ async-await แทน
+เงื่อนไข
+- ใช้ try-catch
+- ให้ log ข้อความ "Start download..." ก่อน await
+- เมื่อเสร็จให้แสดง "Finish!" */
+
+function downloadFile(isNetworkError) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(isNetworkError) {
-                reject("Network Error!")
-            } else {
-                resolve("Download success!")
-            }
+            if (isNetworkError) reject("Network Error!");
+            else resolve("Download success!");
         }, 2000);
-    })
+    });
 }
 
-console.log("Start...");
+async function run() {
+    console.log("Start download...");
 
-downloadFile(false)
-    .then((result) => {
+    try {
+        const result = await downloadFile(false);
         console.log(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    .finally(() => {
-        console.log("Finish");
-    });
+    } catch (error) {
+        console.log(error);
+    }
+
+    console.log("Finish!");
+}
+
+run();
