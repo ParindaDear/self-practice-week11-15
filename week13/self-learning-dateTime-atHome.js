fd8c9b5c0db9ed4d71236286af0fd7c42158294a
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-11-25 21:44:38 +0700

    Learnig about datetime at home

--- week13/self-learning-dateTime-atHome.js
+++ week13/self-learning-dateTime-atHome.js
@@ -24,3 +24,31 @@ const options = Intl.DateTimeFormat().resolvedOptions()
 console.log("locale:", options.locale) //locale: th-TH
 console.log("timeZone:", options.timeZone) //timeZone: Asia/Bangkok
 console.log("hourCycle:", options.hourCycle) //hourCycle: undefined
+
+/*3. ให้ใช้ .toString()  .toISOString()  .toLocaleString()
+เพื่อแสดงวันเวลาใน 3 รูปแบบและอธิบาย “ความต่าง” ของแต่ละแบบ
+กำหนดวันที่: const d = new Date("2025-02-02T11:15:00.150Z") */
+const d = new Date("2025-02-02T11:15:00.150Z");
+
+console.log("toString():", d.toString());
+console.log("toISOString():", d.toISOString());
+console.log("toLocaleString():", d.toLocaleString());
+
+/* 4. ให้ใช้:
+toLocaleString("th-TH", { 
+  dateStyle: "full", 
+  timeStyle: "short" 
+})
+กับวันที่: "2025-12-31T23:30:00Z"
+แล้วดูว่า → จะถูกเลื่อนไป วันถัดไป หรือไม่
+เพราะ timezone ไทยคือ UTC+7*/
+const d4 = new Date("2025-12-31T23:30:00Z");
+
+const result = d4.toLocaleString("th-TH", {
+  dateStyle: "full",
+  timeStyle: "short",
+  timeZone: "Asia/Bangkok",
+});
+
+console.log(result);
+//วันพฤหัสบดีที่ 1 มกราคม พ.ศ. 2569 เวลา 06:30
